a0ac01c5d9b7c96bab7f81be45dd08178252f1be
shivkumar10863 <shiv.1086395@gmail.com>
2026-03-15 08:42:05 +0530

    Initial commit

---
+++ create_model.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import cv2
+import numpy as np
+from torchvision.datasets import MNIST as mn
+from torchvision import transforms
+from torch.utils.data import DataLoader as dl
+transform=transforms.Compose([
+    transforms.Resize((100,100)),
+    transforms.RandomAffine(
+        degrees=15,
+        translate=(0.2, 0.2),
+        scale=(0.7, 1.3)
+    ),
+    transforms.ColorJitter(
+        brightness=0.4,
+        contrast=0.4
+    ),
+    transforms.GaussianBlur(3, sigma=(0.1, 2.0)),
+    transforms.ToTensor(),
+    transforms.RandomErasing(p=0.3)
+])
+device=torch.device("cuda"if torch.cuda.is_available() else"cpu")
+print("Using device:",device)
+x_tr=mn(
+    root='data',
+    train=True,
+    download=True,
+    transform=transform)
+x_te=mn(
+    root='data',
+    train=False,
+    download=True,
+    transform=transform)
+tr_l=dl(x_tr,batch_size=32,shuffle=True)
+te_l=dl(x_te,batch_size=32,shuffle=False) 
+class my_model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer=nn.Sequential(
+            nn.Conv2d(1,8,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(8,16,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2))
+        self.layer0=nn.Sequential(
+            nn.Linear(16*25*25,5000),
+            nn.ReLU(),
+            nn.Linear(5000,1000),
+            nn.ReLU(),
+            nn.Linear(1000,500),
+            nn.ReLU(),
+            nn.Linear(500,100),
+            nn.ReLU(),
+            nn.Linear(100,50),
+            nn.ReLU(),
+            nn.Linear(50,10))
+    def forward(self,x):
+        x=self.layer(x)
+        x=torch.flatten(x,1)
+        x=self.layer0(x)
+        return x
+model=my_model().to(device)
+loss_fn=nn.CrossEntropyLoss()
+optimizer=optim.Adam(model.parameters(),lr=0.001)
+epochs=3
+for epoch in range(epochs):
+    total_loss=0
+    model.train()
+    for image,lebel in tr_l:
+        image=image.to(device)
+        lebel=lebel.to(device)
+        optimizer.zero_grad()
+        y_lebel=model(image)
+        loss=loss_fn(y_lebel,lebel)
+        loss.backward()
+        optimizer.step()
+        total_loss+=loss.item()
+    print(f"Epoch {epoch+1}, Loss: {total_loss/len(tr_l)}")
+torch.save(model.state_dict(),'Number_Detection.pth')
+model.eval()
+correct=0
+total=0
+with torch.no_grad():
+    for image,target in te_l:
+        image=image.to(device)
+        target=target.to(device)
+        y_p=model(image)
+        predicted=y_p.argmax(dim=1)
+        total+=target.size(0)
+        correct+=(predicted==target).sum().item()
+accuracy=100*correct/total
+print(f"Test Accuracy: {accuracy:.2f}%")
+index=800
+image,target=x_te[index]
+img=(image.squeeze().cpu().numpy() * 255).astype(np.uint8)
+img=cv2.resize(img,(1400,700))
+with torch.no_grad():
+        image=image.unsqueeze(0).to(device)  
+        label=model(image)
+        predicted_label=label.argmax(dim=1).item()
+img=cv2.putText(img,f"Predicted Label: {predicted_label}  True Label: {target}",(50,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
+cv2.imshow("Image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print("index:", index)
+print(f"True Label: {target}")
+print(f"Predicted Label: {predicted_label}")
---
+++ main_code.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import cv2
+import streamlit as st
+import numpy as np
+import asyncio
+import edge_tts
+import pygame
+a=True
+b=d=1
+c=0
+sk=np.array([[0,-1,0],[-1,5,-1],[0,-1,0]])
+st.title("Real-Time Handwritten Number Detection")
+st.subheader('Enter one digit at a time in camera view Write the number big with thiker pen')
+cam=cv2.VideoCapture(0)
+indi=cv2.VideoCapture("Speak_Indicator.webm")
+framewindow=st.empty()
+write_window=st.empty()
+device=torch.device("cuda"if torch.cuda.is_available() else"cpu")
+print("Using device:",device)
+async def speak(text):
+    communicate=edge_tts.Communicate(text,"en-US-AriaNeural")
+    await communicate.save("speak.mp3")
+    pygame.mixer.init()
+    pygame.mixer.music.load("speak.mp3")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        framewindow.image(frame0,width=100)
+        pygame.time.Clock().tick(10)
+    pygame.mixer.quit()
+class my_model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer=nn.Sequential(
+            nn.Conv2d(1,8,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(8,16,kernel_size=3,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2))
+        self.layer0=nn.Sequential(
+            nn.Linear(16*25*25,5000),
+            nn.ReLU(),
+            nn.Linear(5000,1000),
+            nn.ReLU(),
+            nn.Linear(1000,500),
+            nn.ReLU(),
+            nn.Linear(500,100),
+            nn.ReLU(),
+            nn.Linear(100,50),
+            nn.ReLU(),
+            nn.Linear(50,10))
+    def forward(self,x):
+        x=self.layer(x)
+        x=torch.flatten(x,1)
+        x=self.layer0(x)
+        return x
+model=my_model().to(device)
+model.load_state_dict(torch.load("Number_Detection.pth", map_location=device))
+model.eval()
+c1,c2=st.columns(2)
+with c1:
+    if st.button("Start"):
+        a=True
+with c2:
+    if st.button("Stop"):
+        a=False
+_,img=indi.read()
+while a:
+    ret,frame=cam.read()
+    ret0,frame0=indi.read()
+    if not ret0:
+        indi.set(cv2.CAP_PROP_POS_FRAMES,0)
+        continue
+    if not ret:
+        print("Failed to grab frame")
+        break
+    image=cv2.resize(frame,(100,100))
+    display=cv2.rectangle(image,(2,2),(98,98),(190,25,90),2)
+    frame=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    _,frame = cv2.threshold(frame,100,255,cv2.THRESH_BINARY_INV)
+    frame=cv2.filter2D(frame,-1,sk)
+    frame=frame/255.0
+    frame = torch.from_numpy(frame).unsqueeze(0).unsqueeze(0).float().to(device)
+    with torch.no_grad():
+        output=model(frame)
+        pred=output.argmax(dim=1).item()
+    framewindow.image(display,width=100)
+    print(pred)
+    write_window.write(f'Predicted Number: {pred}')
+    if b==d or b==pred:
+        b=pred
+        c=c+1
+    else:
+        b=d=1
+        c=0
+    if c==108:
+        asyncio.run(speak(f'Predicted Number: {pred}'))
+        b=d=1
+        c=0
+cam.release()
+indi.release()
